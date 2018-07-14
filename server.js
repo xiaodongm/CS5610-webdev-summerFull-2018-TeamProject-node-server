114cfbbaab430462ad77313a50466b8e065c2bbd
ylxz0073 <jiapengtao@yahoo.com>
2018-07-14 14:16:51 -0700

    add initial cors anywhere server

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+//
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// });
+//
+// app.listen(3000);
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+
+// cors anywhere server
+
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
-
-app.listen(3000);
