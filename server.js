6146573c877e5b75c6dc5a33dae452b88981ab67
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-08-14 00:55:53 -0700

    updates to deploy on heroku.

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    // res.header("Access-Control-Allow-Origin", "https://husky-camp-app.herokuapp.com");
+    // res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "https://husky-camp-app.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
